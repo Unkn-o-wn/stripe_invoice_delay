de70418e902f1a35ba1dd1f48a6209eb29539e89
Daniel Savchuk <d.savchuk@deepvision.team>
2025-07-28 21:51:09 +0300

    Change default port to 8080

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
     bufferLogs: true,
   });
   app.useLogger(app.get(Logger));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
